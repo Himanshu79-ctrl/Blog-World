f97ad04b9c85f7d1b4809c09a6d37671296d336f
Himanshu79-ctrl <709himanshu@gmail.com>
2026-02-01 16:24:42 +0530

    posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from blogs.models import Blog, Category
 
 def home(request):
-    return render(request, 'home.html')
+    catagories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
+    posts = Blog.objects.filter(is_featured=False, status='Published')
+    print(posts)
+    context = {
+        'catagories': catagories,
+        'featured_posts': featured_posts,
+        'posts': posts,
+    }
+    return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Blog, Category
 # Create your views here.
+
+def posts_by_category(request, category_id):
+    #fetch the post that belongs to the category witt the id category_id
+    post = Blog.objects.filter(category__id=category_id, status='Published')
+    try:
+        category = Category.objects.get(id=category_id)
+    except:
+        #if the category does not exist, return to the home page
+        return redirect('home')
+    #category = get_object_or_404(Category, id=category_id)
+    context={
+        'post': post,
+        'category': category,
+    }
+    return render(request, 'posts_by_category.html', context)
