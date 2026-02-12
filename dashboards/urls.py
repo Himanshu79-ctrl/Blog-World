a37542847ab0d4b0204aa542c006d6c7e0c320ec
Himanshu79-ctrl <709himanshu@gmail.com>
2026-02-12 23:08:18 +0530

    Post crud operations/features

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -3,8 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    #category crud urls
     path('categories/', views.categories, name='categories'),
     path('category/add/', views.add_category, name='add_category'),  
     path('category/edit/<int:pk>/', views.edit_category, name='edit_category'),
-    path('category/delete/<int:pk>/', views.delete_category, name='delete_category'),       
+    path('category/delete/<int:pk>/', views.delete_category, name='delete_category'),    
+
+    #blog crud urls
+    path('posts/', views.posts, name='posts'), 
+    path('post/add/', views.add_post, name='add_post'),  
+    path('post/edit/<int:pk>/', views.edit_post, name='edit_post'),
+    path('post/delete/<int:pk>/', views.delete_post, name='delete_post'),
 ]
--- dashboards/views.py
+++ dashboards/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from blogs.models import Blog, Category
 from django.contrib.auth.decorators import login_required
 
-from .forms import CategoryForm  
+from .forms import BlogPostForm, CategoryForm  
+from django.template.defaultfilters import slugify
 
 
 # Create your views here.
@@ -53,3 +54,56 @@ def delete_category(request, pk):
     category = Category.objects.get(id=pk)
     category.delete()
     return redirect('categories')
+
+
+
+def posts(request):
+    posts = Blog.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'dashboard/posts.html', context)
+
+def add_post(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)  # Create a Blog instance but don't save to the database yet
+            post.author = request.user  # Set the author to the currently logged-in user
+            post.save()  # Save the Blog instance to the database to generate an ID that id is used to generate unique slug
+            title = form.cleaned_data['title']  # Get the title from the form data
+            post.slug = slugify(title) + '-' + str(post.id)  # Generate a slug from the title and append post ID to ensure uniqueness   
+            post.save() 
+            return redirect('posts')
+        else:
+            print(form.errors)
+
+    forms = BlogPostForm()
+    context = {
+        'forms': forms,
+    }
+    return render(request, 'dashboard/add_post.html', context)
+
+
+def edit_post(request, pk):
+    post = Blog.objects.get(id=pk)
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES, instance=post) #request.data = new values, instance = old values
+        if form.is_valid():
+            post = form.save()
+            title = form.cleaned_data['title']  # Get the title from the form data
+            post.slug = slugify(title) + '-' + str(post.id)  # Generate a slug from the title and append post ID to ensure uniqueness   
+            post.save()      
+            return redirect('posts')
+    forms = BlogPostForm(instance=post)
+    context = {
+        'forms': forms,
+        'post': post,
+    }
+    return render(request, 'dashboard/edit_post.html', context)
+
+
+def delete_post(request, pk):
+    post = Blog.objects.get(id=pk)
+    post.delete()
+    return redirect('posts')
